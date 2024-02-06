8adcc2b8075a025eaf09f5b3949e2107acfcf2a7
EvenceEliassaint <ev413623@ucf.edu>
2024-02-05 19:53:06 -0500

    Created quote app

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # excercise-3
+"All around the Sta-actus plant, the stalker chased the bandit. The stalker thought 'twas all in fun. POP! [huge explosion] goes the bandit." ~ Tiny Tina Borderlands 2
---
+++ index.js
@@ -0,0 +1,8 @@
+let quotations = [
+    "I wonder what it's like to have a belly button ~ ClapTrap",
+    "I'm gonna eat so many crumpets, it's gonna be a crump-pocalypse! ~ Tinya Tina",
+    "There's no barbeque until I say there's a barbeque. ~ Krieg"
+  ];
+  
+  console.log(quotes[Math.floor(Math.random()*3)])
+  
